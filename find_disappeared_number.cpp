c0ed7bfe27f9c616e135f2b2a7885e95004bafd3
Kartik <jangirkartik667799@gmail.com>
2025-07-01 12:13:02 +0530

    .

---
+++ find_disappeared_number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> vec=`
+    obj.findDisappearedNumbers()
+}
